9288524d655b6f7a24422186ec10bc6bfe06ea77
YEONIHYEON <158719845+DISNOTACAT@users.noreply.github.com>
2024-10-22 05:29:25 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 100 ms, Memory: 14232 KB -BaekjoonHub

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
@@ -1,117 +1,36 @@
-import java.io.*;
-import java.util.StringTokenizer;
-
-// 런타임 에러 (StringIndexOutOfBounds)
-//public class B2941 {
-//
-//        public static void solution() throws IOException {
-//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-//
-//        StringBuilder str = new StringBuilder(br.readLine());
-//        int length = str.length();
-//        int idx = 0;
-//        int cnt = 0;
-//
-//        while(idx < length) {
-//
-//            if(idx == (length - 1)) {
-//                cnt++;
-//                break;
-//            }
-//
-//            String temp = str.substring(idx,idx+2);
-//            switch(temp) {
-//                case "c=" :
-//                    cnt++; idx +=2; break;
-//                case "c-" :
-//                    cnt++; idx +=2; break;
-//                case "d-" :
-//                    cnt++; idx +=2; break;
-//                case "lj" :
-//                    cnt++; idx +=2; break;
-//                case "nj" :
-//                    cnt++; idx +=2; break;
-//                case "s=" :
-//                    cnt++; idx +=2; break;
-//                case "z=" :
-//                    cnt++; idx +=2; break;
-//                case "dz" :
-//                    if(str.toString().charAt(idx+2) == '=') {
-//                        cnt++; idx +=3;
-//                        break;
-//                    }
-//                default:
-//                    cnt++; idx +=1;
-//                    break;
-//            }
-//        }
-//
-//        System.out.println(cnt);
-//
-//        bw.flush();
-//        bw.close();
-//        br.close();
-//    }
-//
-//    public static void main(String[] args) throws IOException {
-//        solution();
-//    }
-//}
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-
-        public static void solution() throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        StringBuilder str = new StringBuilder(br.readLine());
-        int length = str.length();
-        int idx = 0;
-        int cnt = 0;
-
-        while(idx < length) {
-
-            if(idx == (length - 1)) {
-                cnt++;
-                break;
-            }
-
-            String temp = str.substring(idx,idx+2);
-            switch(temp) {
-                case "c=" :
-                    cnt++; idx +=2; break;
-                case "c-" :
-                    cnt++; idx +=2; break;
-                case "d-" :
-                    cnt++; idx +=2; break;
-                case "lj" :
-                    cnt++; idx +=2; break;
-                case "nj" :
-                    cnt++; idx +=2; break;
-                case "s=" :
-                    cnt++; idx +=2; break;
-                case "z=" :
-                    cnt++; idx +=2; break;
-                case "dz" :
-                    if(idx < (length - 2) && str.toString().charAt(idx+2) == '=') {
-                        cnt++; idx +=3;
-                        break;
-                    }
-                default:
-                    cnt++; idx ++;
-                    break;
-            }
-        }
-
-        System.out.println(cnt);
-
-        bw.flush();
-        bw.close();
-        br.close();
-    }
-
     public static void main(String[] args) throws IOException {
-        solution();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+
+        if (str.contains("c=")){
+            str = str.replace("c=", ",");
+        }
+        if (str.contains("c-")){
+            str = str.replace("c-", ",");
+        }
+        if (str.contains("dz=")){
+            str = str.replace("dz=", ",");
+        }
+        if (str.contains("z=")){
+            str = str.replace("z=", ",");
+        }
+        if (str.contains("d-")){
+            str = str.replace("d-", ",");
+        }
+        if (str.contains("lj")){
+            str = str.replace("lj", ",");
+        }
+        if (str.contains("nj")){
+            str = str.replace("nj", ",");
+        }
+        if (str.contains("s=")){
+            str = str.replace("s=", ",");
+        }
+        System.out.println(str.length());
     }
 }
