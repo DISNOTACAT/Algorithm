d88dfcd69f698f618282346c0bab30308a7f3e03
YEONIHYEON <158719845+DISNOTACAT@users.noreply.github.com>
2024-12-28 14:46:40 +0900

    [level 3] Title: 네트워크, Time: 0.19 ms, Memory: 86.6 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,50 +1,36 @@
 import java.util.*;
 
 class Solution {
-  public int solution(int n, int[][] computers) {
 
-    ArrayList<Integer>[] list = new ArrayList[n];
-    for (int i = 0; i < n; i++) {
-      list[i] = new ArrayList<>();
-    }
+  static boolean[] visit;
 
-    for(int i = 0; i < computers.length; i++) {
+  public static int solution(int n, int[][] computers) {
 
-      for (int j = 0; j < computers[i].length; j++) {
-
-        if (computers[i][j] == 1) {
-          list[i].add(j);
-        }
-      }
-    }
-
-    boolean visit[] = new boolean[computers.length];
+    visit = new boolean[n];
     int count = 0;
 
+    for(int i = 0; i < n; i++) { // 각 노드마다 순회
 
-    for(int i = 0; i < computers.length; i++) { // 각 노드마다 순회
-
-      if(visit[i]) continue;  // 이전에 연결 확인한 노드면 패스
-
-      Queue<Integer> queue = new LinkedList<>();
-      queue.add(list[i].get(0));
-      count++;
-
-      while(!queue.isEmpty()) { // 연결된 곳들을 순회
-
-        int current = queue.poll();
-        visit[current] = true;
-
-        if(list[current].isEmpty()) continue; // 리프 노드면 패스
-
-        for(int j = 0; j < list[current].size(); j++) {
-          if(!visit[list[current].get(j)]) {
-            queue.add(list[current].get(j));
-          }
-        }
+      if(!visit[i]) {
+        dfs(computers, i);
+        count++;
       }
     }
+
     return count;
 
   }
+
+  public static void dfs(int[][] computers, int i) {
+
+    // i 번 컴퓨터에 연결된 컴퓨터 체크
+    visit[i] = true;
+
+    for(int j = 0; j < computers[i].length; j++) {
+      if(computers[i][j] == 1 && !visit[j]) {
+        dfs(computers, j);
+      }
+    }
+  }
+  
 }
