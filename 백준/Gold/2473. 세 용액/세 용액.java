867bc99a1e1bb669063bdce09ed03f898e17c112
YEONIHYEON <158719845+DISNOTACAT@users.noreply.github.com>
2025-04-23 11:04:49 +0900

    [Gold III] Title: 세 용액, Time: 200 ms, Memory: 15928 KB -BaekjoonHub

--- 백준/Gold/2473. 세 용액/세 용액.java
+++ 백준/Gold/2473. 세 용액/세 용액.java
@@ -1,91 +1,56 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
-  public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    int N = Integer.parseInt(br.readLine());
-    StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-
-    long[] arr = new long[N];
-    for(int i = 0; i < N; i++){
-      arr[i] = Integer.parseInt(st.nextToken());
-    }
-    Arrays.sort(arr);
-
-    long[] answer = solution(arr);
-    Arrays.sort(answer);
-
-    for(long l : answer) {
-      System.out.print(l + " ");
-    }
-  }
-
-  private static long[] solution(long[] arr) {
-
-    int fixedIndex = 0;
-    long[] answer = new long[3];
-    long min = Long.MAX_VALUE;
-
-
-    while(fixedIndex < arr.length) {
-
-      int leftIndex = 0;
-      int rightIndex = arr.length - 1;
-
-      while(arr[leftIndex] < arr[rightIndex]) {
-
-        if(leftIndex == fixedIndex) {
-          leftIndex++;
-          continue;
+        long[] arr = new long[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = Long.parseLong(st.nextToken());
         }
+        Arrays.sort(arr);
 
-        if(rightIndex == fixedIndex) {
-          rightIndex--;
-          continue;
+        long[] answer = solution(arr);
+
+        Arrays.sort(answer);
+        for (long l : answer) {
+            System.out.print(l + " ");
         }
-
-
-        long sum = arr[fixedIndex] + arr[leftIndex] + arr[rightIndex];
-
-        if(sum == 0) return new long[]{arr[fixedIndex], arr[leftIndex], arr[rightIndex]};
-
-        if(Math.abs(sum) < min) {
-          answer[0] = arr[fixedIndex];
-          answer[1] = arr[rightIndex];
-          answer[2] = arr[leftIndex];
-          min = Math.abs(sum);
-        }
-
-        if(sum > 0) {
-
-          if(arr[rightIndex] < 0) {
-            leftIndex++;
-            continue;
-          }
-
-          rightIndex--;
-        }
-
-        if(sum < 0) {
-
-          if(arr[leftIndex] > 0) {
-            rightIndex--;
-            continue;
-          }
-
-          leftIndex++;
-        }
-      }
-      fixedIndex++;
     }
 
-    return answer;
-  }
+    private static long[] solution(long[] arr) {
+        int N = arr.length;
+        long min = Long.MAX_VALUE;
+        long[] answer = new long[3];
 
+        for (int i = 0; i < N - 2; i++) {
+            int left = i + 1;
+            int right = N - 1;
+
+            while (left < right) {
+                long sum = arr[i] + arr[left] + arr[right];
+
+                if (Math.abs(sum) < min) {
+                    min = Math.abs(sum);
+                    answer[0] = arr[i];
+                    answer[1] = arr[left];
+                    answer[2] = arr[right];
+                }
+
+                if (sum > 0) {
+                    right--;
+                } else if (sum < 0) {
+                    left++;
+                } else {
+                    // sum == 0이면 가장 가까운 값이므로 바로 리턴
+                    return new long[]{arr[i], arr[left], arr[right]};
+                }
+            }
+        }
+        return answer;
+    }
 }
